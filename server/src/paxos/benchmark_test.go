8e6604d1de7d2466871091bfe5bf899620624125
Stephanie Yu <styu314@gmail.com>
2014-05-03 15:15:47 -0400

    Added tests with conflicts

--- server/src/paxos/benchmark_test.go
+++ server/src/paxos/benchmark_test.go
@@ -24,11 +24,15 @@ func ndecidedBenchmark(b *testing.B, pxa []*Paxos, seq int) int {
 
 func waitnBenchmark(b *testing.B, pxa[]*Paxos, seq int, wanted int) {
   to := time.Millisecond
-  for iters := 0; iters < 30; iters++ {
+  //for iters := 0; iters < 100; iters++ {
+  for {
     if ndecidedBenchmark(b, pxa, seq) >= wanted {
       break
     }
     time.Sleep(to)
+    // if to < time.Second {
+    //   to *= 2
+    // }
   }
   nd := ndecidedBenchmark(b, pxa, seq)
   if nd < wanted {
@@ -51,6 +55,48 @@ func BenchmarkRoundtrip(b *testing.B) {
   b.ResetTimer()
   for i := 0; i < b.N; i++ {
     pxa[0].Start(0, "hello")
+    waitnBenchmark(b, pxa, 0, npaxos)
+  }
+}
+
+func BenchmarkConflictThree(b *testing.B) {
+  const npaxos = 3
+  var pxa []*Paxos = make([]*Paxos, npaxos)
+  var pxh []string = make([]string, npaxos)
+  defer cleanup(pxa)
+
+  for i := 0; i < npaxos; i++ {
+    pxh[i] = port("basic", i)
+  }
+  for i := 0; i < npaxos; i++ {
+    pxa[i] = Make(pxh, i, nil)
+  }
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+    for j := 0; j < npaxos; j++ {
+      pxa[j].Start(0, j*100)
+    }
+    waitnBenchmark(b, pxa, 0, npaxos)
+  }
+}
+
+func BenchmarkConflictFive(b *testing.B) {
+  const npaxos = 5
+  var pxa []*Paxos = make([]*Paxos, npaxos)
+  var pxh []string = make([]string, npaxos)
+  defer cleanup(pxa)
+
+  for i := 0; i < npaxos; i++ {
+    pxh[i] = port("basic", i)
+  }
+  for i := 0; i < npaxos; i++ {
+    pxa[i] = Make(pxh, i, nil)
+  }
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+    for j := 0; j < npaxos; j++ {
+      pxa[j].Start(0, j*100)
+    }
     waitnBenchmark(b, pxa, 0, npaxos)
   }
 }
