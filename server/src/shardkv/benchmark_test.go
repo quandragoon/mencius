0b48708adc864c8731b1cf4140bdbf23c86477b9
Stephanie Yu <styu314@gmail.com>
2014-05-08 00:47:20 -0400

    Benchmark tests for shardkv

---
+++ server/src/shardkv/benchmark_test.go
@@ -0,0 +1,79 @@
+package shardkv
+
+import "testing"
+import "shardmaster"
+import "math/rand"
+import "strconv"
+import "time"
+
+func BenchmarkPut(b *testing.B) {
+  smh, gids, ha, _, clean := setup("basic", false)
+  defer clean()
+
+  mck := shardmaster.MakeClerk(smh)
+  mck.Join(gids[0], ha[0])
+
+  ck := MakeClerk(smh)
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+    ck.Put("a", "x")
+  }
+}
+
+func BenchmarkGet(b *testing.B) {
+  smh, gids, ha, _, clean := setup("basic", false)
+  defer clean()
+
+  mck := shardmaster.MakeClerk(smh)
+  mck.Join(gids[0], ha[0])
+
+  ck := MakeClerk(smh)
+  ck.Put("a", "x")
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+    ck.Get("a")
+  }
+}
+
+func BenchmarkPutHash(b *testing.B) {
+  smh, gids, ha, _, clean := setup("basic", false)
+  defer clean()
+
+  mck := shardmaster.MakeClerk(smh)
+  mck.Join(gids[0], ha[0])
+
+  ck := MakeClerk(smh)
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+    ck.PutHash("a", "b")
+  }
+}
+
+func BenchmarkGetAfterJoin(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    b.StopTimer()
+    smh, gids, ha, _, clean := setup("basic", false)
+
+    mck := shardmaster.MakeClerk(smh)
+    mck.Join(gids[0], ha[0])
+
+    ck := MakeClerk(smh)
+    keys := make([]string, 10)
+    vals := make([]string, len(keys))
+    for j := 0; j < len(keys); j++ {
+      keys[j] = strconv.Itoa(rand.Int())
+      vals[j] = strconv.Itoa(rand.Int())
+      ck.Put(keys[j], vals[j])
+    }
+    b.StartTimer()
+    mck.Join(gids[1], ha[1])
+    time.Sleep(250 * time.Millisecond)
+    ck.Get(keys[5])
+    b.StopTimer()
+    clean()
+  }
+}
+
+// Reconfigs
+// PutHash
+// anything with outstanding RPC
